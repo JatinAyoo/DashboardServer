a16eab64f5fe90da0de561ff0fd0b90729af0d21
JatinAyoo <jatin9898yadav@gmail.com>
2024-05-12 04:09:00 +0530

    changes2

--- app.js
+++ app.js
@@ -27,15 +27,31 @@ app.use(cors({
 app.use("/api/data", reportRouter)
 
 app.get("/", (req, res) => {
-    return res.send(`<div style = "background:magenta;padding:100px;"><h2>Welcome to Data Virtualization Server</h2>
-    <p>Below are the some examples of supported routes...</p>
-        <div><ul>
-            <li>GET all data from the database - /api/data</li>
-            <li>GET data filtered by year - /api/data/year/:year</li>
-            <li>GET data filtered by region - /api/data/region/:region</li>
-            <li>Much more...</li>
-        </ul></div>
-    </div>`)
+    return res.send(
+        `<div style="background: linear-gradient(to right, #4e54c8, #8f94fb); color: #fff; padding: 50px; font-family: Arial, sans-serif; border-radius: 10px; box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1); max-width: 600px; margin: 0 auto;">
+    <h2 style="font-size: 2rem; margin-bottom: 20px;">Welcome to Data Virtualization Server</h2>
+    <p style="font-size: 1.2rem; margin-bottom: 20px;">Below are some examples of supported routes...</p>
+    <div>
+        <ul style="padding-left: 20px;">
+            <li style="font-size: 1rem; margin-bottom: 10px;">GET all data from the database - /api/data</li>
+            <li style="font-size: 1rem; margin-bottom: 10px;">GET data filtered by year - /api/data/year/:year</li>
+            <li style="font-size: 1rem; margin-bottom: 10px;">GET data filtered by region - /api/data/region/:region</li>
+            <li style="font-size: 1rem; margin-bottom: 10px;">Much more...</li>
+        </ul>
+    </div>
+</div>`
+
+    //     `<div class="content" style = "background:magenta;padding:100px;"><h2>Welcome to Dashboard Server</h2>
+    // <p>Below are the some examples of supported routes...</p>
+    //     <div><ul>
+    //         <li>GET all data from the database - /api/data</li>
+    //         <li>GET data filtered by year - /api/data/year/:year</li>
+    //         <li>GET data filtered by region - /api/data/region/:region</li>
+    //         <li>Much more...</li>
+    //     </ul></div>
+    // </div>`
+
+)
 })
 
 app.listen(PORT, (req, res) => {
